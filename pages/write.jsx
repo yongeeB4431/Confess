d8a8a7b909f78d00e701dac73e9d022f99eef426
agunbiadebasit5950@gmail.com <agunbiadebasit5950@gmail.com>
2022-11-01 14:15:50 +0100

    fifth deployment

--- pages/write.jsx
+++ pages/write.jsx
@@ -1,31 +1,28 @@
-import Input from '../component/Write/Write'
-import { server } from '../config';
+import Input from "../component/Write/Write";
+import { server } from "../config";
 
-function Write({editConfession}){
-return (
-<Input editConfession={editConfession}/>
-)
+function Write({ editConfession }) {
+  return <Input editConfession={editConfession} />;
 }
-export async function getServerSideProps(context){
-	const {req, res} = context
-	var hostName = req.headers.host
-try{
-let id = context.query.id;
-if(id){
-	const data = await fetch(`${server}${hostName}/api/confession/findOne/${id}`)
-	let editConfession = await data.json()
-	res.setHeader('Cache-Control', 'public s-manage=10, stale-while-revalidate=59')
-	return{
-		props: {editConfession}
-	} 
-	}
-		return{
-			props: {editConfession: {}}
-	
-}
-	
-}catch(err){
-console.log(err)
-}
+export async function getServerSideProps(context) {
+  const { req, res } = context;
+  var hostName = req.headers.host;
+  try {
+    let id = context.query.id;
+    if (id) {
+      const data = await fetch(
+        `${server}${hostName}/api/confession/findOne/${id}`
+      );
+      let editConfession = await data.json();
+      return {
+        props: { editConfession },
+      };
+    }
+    return {
+      props: { editConfession: {} },
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
 export default Write;
